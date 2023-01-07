1bff5e3ba5ad09ae3dd347e7824cf54c5dcd617c
josemoracard <jose.cardinales@gmail.com>
2023-01-07 10:29:28 +0000

    login rama

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,26 +1,47 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { Context } from "../store/appContext";
+import { useNavigate } from "react-router-dom";
 import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
+  const { store, actions } = useContext(Context);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
-	);
+  const handleClick = async () => {
+    const login = await actions.login(email, password)
+    console.log(login)
+    if (login) {
+      navigate("/demo")
+    };
+  };
+
+  return (
+    <div className="text-center mt-5">
+      <h1>Hello Rigo!!</h1>
+      <p>
+        <img src={rigoImageUrl} />
+      </p>
+      <input onChange={(e) => setEmail(e.target.value)} placeholder="Email"></input>
+      <input onChange={(e) => setPassword(e.target.value)} placeholder="Password"></input>
+
+      <button onClick={handleClick}>Login</button>
+      <button onClick={() => actions.logout()}>Logout</button>
+      {
+        !store.token ? "No estoy logeado" : "estoy logeado"
+      }
+      <div className="alert alert-info">
+        {store.message ||
+          "Loading message from the backend (make sure your python backend is running)..."}
+      </div>
+      <p>
+        This boilerplate comes with lots of documentation:{" "}
+        <a href="https://start.4geeksacademy.com/starters/react-flask">
+          Read documentation
+        </a>
+      </p>
+    </div>
+  );
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -22,14 +22,14 @@ const getState = ({ getStore, getActions, setStore }) => {
 			},
 
 			getMessage: async () => {
-				try{
+				try {
 					// fetching data from the backend
 					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
 					return data;
-				}catch(error){
+				} catch (error) {
 					console.log("Error loading message from backend", error)
 				}
 			},
@@ -46,6 +46,76 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
+			},
+			login: async (email, password) => {
+				try {
+					// fetching data from the backend
+					const resp = await fetch(process.env.BACKEND_URL + "/api/login",
+						{
+							headers: {
+								"Content-Type": "application/json"
+							},
+							method: "POST",
+							body: JSON.stringify({
+								"email": email,
+								"password": password
+							})
+						}
+					)
+					const data = await resp.json()
+					setStore({
+						token: data.token,
+						user: data.user
+					})
+					localStorage.setItem("token", data.token)
+					// don't forget to return something, that is how the async resolves
+					if (data.token) {
+						return true;
+					}
+					return false;
+				} catch (error) {
+					console.log("Error loading message from backend", error)
+					return false
+				}
+			},
+
+			refreshToken: () => {
+				setStore({
+					token: localStorage.getItem("token") ? localStorage.getItem("token") : null
+				})
+
+			},
+
+			private: async () => {
+				const store = getStore()
+				try {
+					// fetching data from the backend
+					const resp = await fetch(process.env.BACKEND_URL + "/api/private",
+						{
+							headers: {
+								"Content-Type": "application/json",
+								"Authorization": `Bearer ${store.token}`
+							},
+							method: "POST",
+						}
+					)
+					const data = await resp.json()
+					setStore({
+						message: data.message
+					})
+					// don't forget to return something, that is how the async resolves
+					if (data.status == 200) {
+						return true;
+					}
+					return false;
+				} catch (error) {
+					console.log("Error loading message from backend", error)
+				}
+			},
+			logout: () => {
+				setStore({
+					token: ""
+				});
 			}
 		}
 	};
