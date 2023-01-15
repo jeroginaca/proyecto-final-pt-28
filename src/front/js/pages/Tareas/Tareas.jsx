34d815265ec16bc54a59a3275c291b6f73b99bd9
miriamvalades <miriamex56@gmail.com>
2023-01-15 17:24:47 +0000

    update todo list

--- src/front/js/pages/Tareas/Tareas.jsx
+++ src/front/js/pages/Tareas/Tareas.jsx
@@ -4,6 +4,7 @@ import TodoForm from "./TodoForm.jsx";
 import "../../../styles/todolist.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPencil } from "@fortawesome/free-solid-svg-icons";
+import { faTrashCan } from "@fortawesome/free-regular-svg-icons";
 import { faFloppyDisk } from "@fortawesome/free-solid-svg-icons";
 import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
 
@@ -21,31 +22,17 @@ function Todo({ todo, index, completeTodo, removeTodo, editTodo }) {
   }
 
   return (
-    <div
-      className="todo"
-      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
-    >
+    <div className="todo" style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}>
+      <button className="btn" onClick={() => completeTodo(index)}>
+        <FontAwesomeIcon icon={faCircleCheck} />
+      </button>   
       {isEditing ? (
-        <input type="text" value={newText} onChange={(
-		e) => setNewText(e.target.value)} />
-) : (
-<span>{todo.text}</span>
-)}
-<div>
+        <input type="text" value={newText} onChange={(e) => setNewText(e.target.value)} /> ) : ( <span>{todo.text}</span>)}
+<div className="botones" >
 {isEditing ? (
-<button className="save btn btn-success" onClick={() => handleSave(index)}><FontAwesomeIcon icon={faFloppyDisk} /></button>
-) : (
-<button className="edit btn btn-warning" onClick={() => handleEdit(index)}><FontAwesomeIcon icon={faPencil} className="fa-edit"/></button>
-)}
-<button
-className="complete btn btn-primary"
-onClick={() => completeTodo(index)}
->
-<FontAwesomeIcon icon={faCircleCheck} />
-</button>
-<button className="remove btn btn-danger" onClick={() => removeTodo(index)}>
-<i class="fa fa-times m-0 p-0"></i>
-</button>
+<button className="save btn btn-success" onClick={() => handleSave(index)}><FontAwesomeIcon icon={faFloppyDisk} /></button> ) : (
+<button className="edit btn btn-warning" onClick={() => handleEdit(index)}><FontAwesomeIcon icon={faPencil} className="fa-edit"/></button>)}
+<button className="remove btn btn-danger" onClick={() => removeTodo(index)}><FontAwesomeIcon icon={faTrashCan} className="fa-trash"/></button>
 </div>
 </div>
 );
