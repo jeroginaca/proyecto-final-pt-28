ef3a6b9aa5358f95f207a7dac9dd6e52eaec5468
jeroginaca <jeroginaca@gmail.com>
2023-01-28 11:49:27 +0000

    holahola

--- src/front/js/pages/Journal/NoteContainer/NoteContainer.jsx
+++ src/front/js/pages/Journal/NoteContainer/NoteContainer.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import NavbarVolver from "../../../component/2nd Navbar/NavbarVolver.jsx";
+import BottomBar from "../../../component/Bottom Bar/BottomBar.jsx";
 import Note from "../Note/Note.jsx";
 import "./NoteContainer.css";
 
@@ -15,28 +17,31 @@ function NoteContainer(props) {
 
   // const notes = reverArray(props.notes); */
   return (
-    <div className="note-container">
-      <h1 className="mb-4">Entradas</h1>
-      <div className="note-container_notes custom-scroll">
-        {props.notes.length > 0 ? (
-          props.notes.map((item, index) => (
-            <Note
-              key={index}
-              id={item.id}
-              color={item.color}
-              date={item.date}
-              note={item.notes}
-              deleteNote={props.deleteNote}
-              updateText={props.updateText}
-              updateNotes={props.updateNotes}
-              setNotes={props.setNotes}
-            />
-          ))
-        ) : (
-          <p>No tienes ninguna entrada.</p>
-        )}
+    <>
+      <div className="note-container">
+        <h1 className="mb-4">Entradas</h1>
+        <div className="note-container_notes custom-scroll">
+          {props.notes.length > 0 ? (
+            props.notes.map((item, index) => (
+              <Note
+                key={index}
+                id={item.id}
+                color={item.color}
+                date={item.date}
+                note={item.notes}
+                deleteNote={props.deleteNote}
+                updateText={props.updateText}
+                updateNotes={props.updateNotes}
+                setNotes={props.setNotes}
+              />
+            ))
+          ) : (
+            <p>No tienes ninguna entrada.</p>
+          )}
+        </div>
       </div>
-    </div>
+      <BottomBar />
+    </>
   );
 }
 
--- src/front/js/pages/Meditacion/Tiempos.jsx
+++ src/front/js/pages/Meditacion/Tiempos.jsx
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import NavbarVolver from "../../component/2nd Navbar/NavbarVolver.jsx";
+import BottomBar from "../../component/Bottom Bar/BottomBar.jsx";
 import TiemposDeMeditacion from "../../component/TiemposDeMeditacion.jsx";
 
 const Tiempos = () => {
   return (
     <div className="meditacion-background">
-      <TiemposDeMeditacion />
+      <>
+        <NavbarVolver />
+        <TiemposDeMeditacion />
+        <BottomBar />
+      </>
     </div>
   );
 };
