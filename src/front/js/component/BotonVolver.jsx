464b3fd3c706b44c86738a84bc07820f47d0d4f8
josemoracard <jose.cardinales@gmail.com>
2023-01-28 12:21:46 +0000

    boton volver

--- src/front/js/component/BotonVolver.jsx
+++ src/front/js/component/BotonVolver.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 
-export const BotonVolver = () => {
+export const BotonVolver = (props) => {
   const navigate = useNavigate();
   return (
     <div>
-      <button onClick={() => navigate(-1)}>Volver</button>
+      <button className={props.clase} onClick={() => navigate(-1)}>
+        Volver
+      </button>
     </div>
   );
 };
