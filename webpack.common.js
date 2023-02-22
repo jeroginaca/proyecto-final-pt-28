922114f65b77f21317fa4bc4a6597674a619c097
josemoracard <jose.cardinales@gmail.com>
2023-02-22 21:26:03 +0000

    favicon fix para render

--- webpack.common.js
+++ webpack.common.js
@@ -60,7 +60,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      favicon: "/workspace/proyecto-final-pt-28/public/Vector.ico",
+      favicon: "4geeks.ico",
       template: "template.html",
     }),
     new Dotenv({ safe: true, systemvars: true }),
